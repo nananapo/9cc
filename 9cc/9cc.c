05fe9d3ebe7ad85de3d8799e0057cfef5df0a402
na <kanapodream@gmail.com>
2022-04-20 20:28:43 +0900

    トークン化

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -1,5 +1,106 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+	TK_RESERVED,
+	TK_NUM,
+	TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+	TokenKind	kind;
+	Token		*next;
+	int			val;
+	char		*str;
+};
+
+Token	*token;
+
+void	error(char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+bool	consume(char op)
+{
+	if (token->kind != TK_RESERVED || token->str[0] != op)
+		return false;
+	token = token->next;
+	return true;
+}
+
+void	expect(char op)
+{
+	if (token->kind != TK_RESERVED || token->str[0] != op)
+		error("'%c'ではありません", op);
+	token = token->next;
+}
+
+int expect_number()
+{
+	int val;
+
+	if (token->kind != TK_NUM)
+		error("数ではありません");
+	val = token->val;
+	token = token->next;
+	return val;	
+}
+
+bool	at_eof()
+{
+	return token->kind == TK_EOF;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
+	Token *tok = calloc(1, sizeof(Token));
+	tok->kind = kind;
+	tok->str = str;
+	cur->next = tok;
+	return tok;
+}
+
+Token	*tokenize(char *p)
+{
+	Token head;
+	head.next = NULL;
+	Token *cur = &head;
+
+	while (*p)
+	{
+		if (isspace(*p))
+		{
+			p++;
+			continue;
+		}
+		if (*p == '+' || *p == '-')
+		{
+			cur = new_token(TK_RESERVED, cur, p++);
+			continue;
+		}
+		if (isdigit(*p))
+		{
+			cur = new_token(TK_NUM, cur, p);
+			cur->val = strtol(p, &p, 10);
+			continue;
+		}
+		error("failed to Tokenize");
+	}
+	
+	new_token(TK_EOF, cur, p);
+	return head.next;
+}
 
 int main(int argc, char **argv)
 {
@@ -11,28 +112,23 @@ int main(int argc, char **argv)
 		fprintf(stderr, "引数の個数が正しくありません");
 		return (1);
 	}
-	str = argv[1];
+
+	token = tokenize(argv[1]);
+
 	printf(".intel_syntax noprefix\n");
 	printf(".global _main\n");
 	printf("_main:\n");
-	printf("    mov rax, %ld\n", strtol(str, &str, 10));
-	while (*str)
+	printf("    mov rax, %d\n", expect_number());
+	while (!at_eof())
 	{
-		if (*str ==  '-')
-			sign = -1;
-		else if (*str == '+')
-			sign = 1;
-		else
+		if (consume('+'))
 		{
-			fprintf(stderr, "予期しない文字です。 '%c'\n", *str);
-			return 1;
+			printf("    add rax, %d\n", expect_number());
+			continue;
 		}
-		str++;
 
-		if (sign == 1)
-			printf("    add rax, %ld\n", strtol(str, &str, 10));
-		else
-			printf("    sub rax, %ld\n", strtol(str, &str, 10));
+		expect('-');
+		printf("    sub rax, %d\n", expect_number());
 	}
 	printf("    ret\n");
 	return (0);
--- 9cc/test.sh
+++ 9cc/test.sh
@@ -20,5 +20,6 @@ assert 0 0
 assert 42 42
 assert 42 "11+12+19"
 assert 1 "11+12+19-41"
-
+assert 1 "11 - 41 + 19 + 12"
+assert 55 "1 +  2 +   3 + 4+5 +  6 + 7    +8+9 + 10"
 echo OK
