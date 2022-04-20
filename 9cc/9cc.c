f8ff1e8907ca3e499bdb6a10bb2b7cdaddcf0551
na <kanapodream@gmail.com>
2022-04-20 21:38:45 +0900

    Nodeのパーサを追加

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -5,19 +5,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Token Token;
+typedef struct Node Node;
+Node	*primary(void);
+
 typedef enum {
 	TK_RESERVED,
 	TK_NUM,
 	TK_EOF,
 } TokenKind;
 
-typedef struct Token Token;
-
 struct Token {
 	TokenKind	kind;
 	Token		*next;
 	int			val;
 	char		*str;
+};
+
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+} NodeKind;
+
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
 };
 
 Token	*token;
@@ -117,6 +134,63 @@ Token	*tokenize(char *p)
 	new_token(TK_EOF, cur, p);
 	return head.next;
 }
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *mul()
+{
+	Node *node = primary();
+	for (;;)
+	{
+		if (consume('*'))
+			node = new_node(ND_MUL, node, primary());
+		else if (consume('/'))
+			node = new_node(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+
+Node *expr()
+{
+	Node *node = mul();
+	for (;;)
+	{
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+Node *primary()
+{
+	if (consume('('))
+	{
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+	return new_node_num(expect_number());
+}
+
 
 int main(int argc, char **argv)
 {
