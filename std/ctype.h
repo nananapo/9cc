a9d76f55eb4db6cd3b250d9c56d76ab8bb7a78b9
na <kanapodream@gmail.com>
2022-06-19 16:40:46 +0900

    ctype.hを作成

---
+++ std/ctype.h
@@ -0,0 +1,95 @@
+/*
+ * C11のctype.h実装
+ * Cロケールのみ対応 
+ */
+
+int	isalnum(int c);
+int	isalpha(int c);
+int	isblank(int c);
+int	iscntrl(int c);
+int	isdigit(int c);
+int	isgraph(int c);
+int	islower(int c);
+int	isprint(int c);
+int	ispunct(int c);
+int	isspace(int c);
+int	isupper(int c);
+int	isxdigit(int c);
+int	tolower(int c);
+int	toupper(int c);
+
+int	isalnum(int c)
+{
+	return (isalpha(c) || isdigit(c));
+}
+
+int	isalpha(int c)
+{
+	return (isupper(c) || islower(c));
+}
+
+int	isblank(int c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+int	iscntrl(int c)
+{
+	return ((0 <= c && c <= 31) || c == 127);
+}
+
+int	isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+int	isgraph(int c)
+{
+	return (iscntrl(c) != 0 && c != ' ');
+}
+
+int	islower(int c)
+{
+	return ('a' <= c && c <= 'z');
+}
+
+int	isprint(int c)
+{
+	return (iscntrl(c) == 0);
+}
+
+int	ispunct(int c)
+{
+	return (isspace(c) == 0 && isalpha(c) == 0);
+}
+
+int	isspace(int c)
+{
+	return (c == ' ' || c == '\f' || c == '\n'
+		|| c == '\r' || c == '\t' || c == '\v');
+}
+
+int	isupper(int c)
+{
+	return ('A' <= c && c <= 'Z');
+}
+
+int	isxdigit(int c)
+{
+	return (('0' <= c && c <= '9')
+		|| ('a' <= c && c <= 'f') || ('A' <= c && c <= 'Z'));
+}
+
+int	tolower(int c)
+{
+	if (isupper(c))
+		return (c - 'A' + 'a');
+	return (c);
+}
+
+int	toupper(int c)
+{
+	if (islower(c))
+		return (c - 'a' + 'A');
+	return (c);
+}
