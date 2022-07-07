3947c9ada6646cbd6e3d9f06e846a916253e059d
na <kanapodream@gmail.com>
2022-07-07 16:41:31 +0900

    stdio.hの定義を変更
    stdinをfopen(dev/stdin)に置き換えていたのを、__stdinpに置き換えるように
    __stdinpはexternした

--- std/stdio.h
+++ std/stdio.h
@@ -6,8 +6,12 @@ void	printf(char *fmt, ...);
 void	fprintf(void *fp, char *fmt, ...);
 void	*fopen(char *path, char *mode);
 
-# define stdin fopen("/dev/stdin", "r")
-# define stdout fopen("/dev/stdout", "a")
-# define stderr fopen("/dev/stderr", "a")
+extern	void *__stdinp;
+extern	void *__stdoutp;
+extern	void *__stderrp;
+
+# define stdin __stdinp
+# define stdout __stdoutp
+# define stderr __stderrp
 
 #endif
