1dfb9deeda9394ae6f21a1dde9b15672ac69ae42
na <kanapodream@gmail.com>
2022-07-29 12:28:00 +0900

    Update stdio.h

--- std/stdio.h
+++ std/stdio.h
@@ -6,6 +6,7 @@ int		printf(char *fmt, ...);
 int		fprintf(void *fp, char *fmt, ...);
 void	*fopen(char *path, char *mode);
 int		sprintf(char *str, char *fmt, ...);
+int		scanf(char *fmt, ...);
 
 #include <stdarg.h>
 void	vfprintf(void *stream, char *format, va_list ap);
