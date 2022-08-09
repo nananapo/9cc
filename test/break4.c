1b77f44fbd23cb75ef634074d04c4c33d28f4aa2
nananapo <kanapo.go@gmail.com>
2022-08-08 21:56:28 -0400

    .

---
+++ test/break4.c
@@ -0,0 +1,9 @@
+int dint(int c);
+
+int main(void)
+{
+        int a = 0;
+        do
+		dint(a < 100);
+        while (a++ < 100);
+}
---
+++ test/global35.c
@@ -0,0 +1,8 @@
+int dint(int a);
+
+int g_a = 1000;
+
+int main(void)
+{
+	dint(g_a);
+}
