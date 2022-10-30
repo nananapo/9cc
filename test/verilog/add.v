ce87196d15a43203c27b81a9fe62f5698240c637
nananapo <kanapodream@gmail.com>
2022-10-30 21:41:06 +0900

    テスト用のプログラムを作成

--- test/verilog/add.v
+++ test/verilog/add.v
@@ -8,11 +8,11 @@ reg [31:0] main_val_a;
 
 always @ (posedge clock, negedge n_reset) begin
 	if (n_reset == 1'b0) begin
-		state <= 32'd0
+		state <= 32'd0;
 	end else begin
 		if (state == 32'd0) begin
-			main_val_a <= 32'd100
-			state <= 32'd1
+			main_val_a <= 32'd100;
+			state <= 32'd1;
 		end else if (state == 32'd1) begin
 			// ?
 		end
